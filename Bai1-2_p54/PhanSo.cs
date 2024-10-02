59b8e3fd67a4a05024c2e00c98e1bc53606f4f56
DuyTin77 <119142567+DuyTin77@users.noreply.github.com>
2024-10-02 09:48:56 +0700

    Add files via upload

---
+++ Bai1-2_p54/PhanSo.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Phanso
+{
+    class PhanSo
+    {
+        private int tu;
+        private int mau;
+        public void nhap()
+        {
+            Console.Write("Nhap tu so: ");
+            this.tu=int.Parse(Console.ReadLine());
+            do
+            {
+                Console.Write("Nhap mau so: ");
+                this.mau=int.Parse(Console.ReadLine());
+            }while(mau == 0);
+        }
+        public void xuat()
+        {
+            if (this.tu == this.mau || this.tu == 0)
+                Console.WriteLine("So nguyen: " + this.tu);
+            else
+                Console.WriteLine("Phan so: " + this.tu + "/" + this.mau);
+        }
+        public PhanSo Cong(PhanSo a, PhanSo b)
+        {
+            PhanSo t = new PhanSo();
+            if(a.mau == b.mau)
+            {
+                t.tu = a.tu + b.tu;
+                t.mau = a.mau;
+                return t;
+            }
+            else
+            {
+                t.tu = a.tu * b.mau + b.tu * a.mau;
+                t.mau = a.mau * b.mau;
+                return t;
+            }
+            
+        }
+        public PhanSo Tru(PhanSo a, PhanSo b)
+        {
+            PhanSo t = new PhanSo();
+            if (a.mau == b.mau)
+            {
+                t.tu = a.tu - b.tu;
+                t.mau = a.mau;
+                return t;
+            }
+            else
+            {
+                t.tu = a.tu * b.mau - b.tu * a.mau;
+                t.mau = a.mau * b.mau;
+                return t;
+            }
+
+        }
+        public PhanSo Nhan(PhanSo a, PhanSo b)
+        {
+            PhanSo t = new PhanSo();
+            t.tu = a.tu * b.tu;
+            t.mau = a.mau * b.mau;
+            return t;
+        }
+        public PhanSo Chia(PhanSo a, PhanSo b)
+        {
+            PhanSo t = new PhanSo();
+            t.tu = a.tu * b.mau;
+            t.mau= a.mau * b.tu;
+            return t;
+        }
+
+        //Nap chong
+        public static PhanSo operator +(PhanSo a, PhanSo b)
+        {
+            PhanSo t= new PhanSo();
+            t = t.Cong(a, b);
+            return t;
+        }
+
+        private int uc(int a, int b)
+        {
+            if (a == 0 || b == 0)
+                return a + b;
+            else
+            {
+                while (a != b)
+                {
+                    if (a > b)
+                        a = a - b;
+                    else
+                        b = b - a;
+                }
+                return a;
+            }
+        }
+        public void RutGon()
+        {
+            int s = uc(this.tu, this.mau);
+            this.tu /= s;
+            this.mau /= s;
+        }
+    }
+}
---
+++ Bai1-2_p54/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Phanso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhanSo a = new PhanSo();
+            PhanSo b = new PhanSo();
+            PhanSo t = new PhanSo();
+            PhanSo t1 = new PhanSo();
+            PhanSo t2 = new PhanSo();
+            PhanSo t3 = new PhanSo();
+
+            Console.WriteLine("--------Phan so 1--------");
+            a.nhap();
+            a.xuat();
+
+            Console.WriteLine("-------Phan so 2-----------");
+            b.nhap();
+            b.xuat();
+
+            Console.Write("Tong 2 phan so la: ");
+            //t = t.Cong(a, b);
+            t = a + b;
+            t.xuat();
+
+            Console.Write("Hieu hai phan so la: ");
+            t1=t1.Tru(a, b);
+            t1.xuat();
+
+            Console.Write("Tich hai phan so la: ");
+            t2=t2.Nhan(a, b);
+            t2.xuat();
+
+            Console.Write("Thuong 2 phan so la: ");
+            t3=t3.Chia(a, b);
+            t3.xuat();
+
+            t.RutGon();
+            Console.Write("\nPhan so sau khi rut gon: ");
+            t.xuat();
+            
+            Console.ReadKey();
+        }
+    }
+}
